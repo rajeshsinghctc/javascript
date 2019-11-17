1b6276c74571f0a4a650310c41caab4531aafdfc
rajeshsinghctc <57046919+rajeshsinghctc@users.noreply.github.com>
2019-11-17 21:49:06 +0530

    Update callBackFunction.js

--- callBackFunction.js
+++ callBackFunction.js
@@ -1,1 +1,15 @@
+let add = function (a,b) {
+    return a+b;
+}
 
+let multiply = function (a,b) {
+    return a*b;
+}
+
+let calculate = function(num1, num2, callback){
+
+    return callback(num1, num2);
+}
+
+console.log(calculate(10, 20, add));
+console.log(calculate(10, 20, multiply));
