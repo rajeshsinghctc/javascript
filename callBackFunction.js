7dbc4d27c1324e81e66518e4f35016b60c0445ca
rajeshsinghctc <57046919+rajeshsinghctc@users.noreply.github.com>
2019-11-18 01:11:59 +0530

    Update callBackFunction.js

--- callBackFunction.js
+++ callBackFunction.js
@@ -1,3 +1,8 @@
+/*
+call back function is a function(anonymous, arrow function) passed into another function as an argument, which is then invoked inside
+outer function to complete some action.
+*/
+
 let add = function (a,b) {
     return a+b;
 }
@@ -13,3 +18,58 @@ let calculate = function(num1, num2, callback){
 
 console.log(calculate(10, 20, add));
 console.log(calculate(10, 20, multiply));
+
+
+/*
+//A.normal callback passing an argument
+let sum = function(a,b){
+    console.log(a+b);
+}
+
+function calculate(num1, num2, callback){
+
+    callback(num1, num2);
+}
+
+calculate(10, 20, sum);
+*/
+
+
+/*
+//B.writing entire function as an argument 
+
+function calculate(num1, num2, callback){
+
+    callback(num1, num2);
+}
+calculate(10, 20, sum = function(a,b){
+    console.log(a+b);
+})
+*/
+
+
+/*
+//C.use of anonymous function(i.e without having name for the function)
+
+function calculate(num1, num2, callback){
+
+    callback(num1, num2);
+}
+calculate(10, 20, function(a,b){
+    console.log(a+b);
+})
+/*
+
+
+
+/*
+//C. use of arrow function
+
+function calculate(num1, num2, callback){
+
+    callback(num1, num2);
+}
+calculate(10, 20, (a, b) => {
+    console.log(a+b);
+})
+*/
