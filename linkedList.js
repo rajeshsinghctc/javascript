9883786156c6411c9e2a7d696d8be7fc025a0210
srajesh <srajesh@zeomega.com>
2020-03-15 16:39:05 +0530

    single linked list operations

--- linkedList.js
+++ linkedList.js
@@ -24,9 +24,84 @@ LinkedList.prototype.insertAtBeginning = function (data) {
     return this.head;
 }
 
-LinkedList.prototype.deleteAtIndex = function (index) {
-    // console.log(node);
+LinkedList.prototype.insertAtEnd = function (data) {
+    let node = new Node(data);
+    let current = this.head;
+    if (this.head == null) {
+        this.head = node;
+    } else {
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = node;
+    }
+    this.size++;
+    return this.head;
+}
 
+LinkedList.prototype.insertAtIndex = function (data, index) {
+    var node = new Node(data);
+    let count = 0;
+    let previous, current;
+    // !this.head same as this.head == null
+    if (!this.head) {
+        this.head = node;
+        this.size++;
+        return this.head;
+    }
+    if (index == 0) { 
+        // if only one element already availabel in list
+        node.next = this.head; 
+        this.head = node; 
+        this.size++;
+    }else{
+        current = this.head;
+        while (count < index) {
+            previous = current;
+            current = current.next;
+            count++;
+            
+        }
+        previous.next = node;
+        node.next = current;
+        this.size++;
+    }
+    
+    return this.head;
+}
+
+LinkedList.prototype.deleteFirstNode = function(){
+    if(this.head == null){
+        return;
+    }
+    this.head = this.head.next;
+    this.size--;
+    return this.head;
+};
+
+LinkedList.prototype.deleteLastNode = function () {
+    let current = this.head;
+    let previous;
+    if(this.head == null){
+        //if no element present in list
+        return;
+    }else if(this.head.next == null){
+        //if only one element is present in list i.e at index=0
+        this.head = this.head.next;
+        this.size--;
+        return;
+    }else  {
+        while(current.next !=null){
+            previous = current;
+            current = current.next;
+        }
+        previous.next = null;
+        this.size--;
+    }
+    return this.head;
+}
+
+LinkedList.prototype.deleteAtIndex = function (index) {
     let current = this.head;
     let previous;
     let count =0;
@@ -43,11 +118,10 @@ LinkedList.prototype.deleteAtIndex = function (index) {
         current= current.next;
         count++;
     }
-    previous.next = previous.next.next;
+    previous.next = current.next; // or previous.next = previous.next.next
     this.size--;
     return this.head;
 }
-
 
 let list = new LinkedList();
 list.insertAtBeginning(9);
