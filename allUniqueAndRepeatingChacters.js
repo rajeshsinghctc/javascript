e15a224018ad441d7ce323456f5227197cec2fd5
srajesh <srajesh@zeomega.com>
2020-03-25 13:09:11 +0530

    same program implementation using Array

--- allUniqueAndRepeatingChacters.js
+++ allUniqueAndRepeatingChacters.js
@@ -11,3 +11,27 @@ for(i=0; i<=arr.length; i++){
         console.log('repeating character--->', arr.charAt(i))
     }
 }
+
+// same program using array
+
+var ar = 'asddsfdjdsjdfdT,@/"';
+var arr = ar.toUpperCase().split('');// array
+for(i=0; i<=arr.length; i++){
+        // print unique characters in a string
+    if(arr.indexOf(arr[i])== arr.lastIndexOf(arr[i])){
+        console.log('non repeating character--->', arr[i])
+    }else{
+        // print duplicate or repeating characters in a string
+        console.log('repeating character--->', arr[i])
+    }
+}var ar = 'asddsfdjdsjdfdT,@/"';
+var arr = ar.toUpperCase().split('');
+for(i=0; i<=arr.length; i++){
+        // print unique characters in a string
+    if(arr.indexOf(arr[i])== arr.lastIndexOf(arr[i])){
+        console.log('non repeating character--->', arr[i])
+    }else{
+        // print duplicate or repeating characters in a string
+        console.log('repeating character--->', arr[i])
+    }
+}
