d7b32655f1bde8373ab62b20de1dc3de33a2a618
srajesh <srajesh@zeomega.com>
2020-03-25 13:00:39 +0530

    print all unique and repeating characters in a string

--- allUniqueAndRepeatingChacters.js
+++ allUniqueAndRepeatingChacters.js
@@ -1,7 +1,13 @@
+
+// code to print all non repeating or unique characters in a string and also print repeating/duplicate characters
 var ar = 'asddsfdjdsjdfdT,@/"';
 var arr = ar.toUpperCase()
 for(i=0; i<=arr.length; i++){
+        // print unique characters in a string
     if(arr.indexOf(arr.charAt(i))== arr.lastIndexOf(arr.charAt(i))){
         console.log('non repeating character--->', arr.charAt(i))
+    }else{
+        // print duplicate or repeating characters in a string
+        console.log('repeating character--->', arr.charAt(i))
     }
 }
