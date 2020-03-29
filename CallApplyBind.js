a92cdcc87eee4065c3a8493f84cc78f11d4079db
srajesh <srajesh@zeomega.com>
2020-03-29 20:33:12 +0530

    added reason why call apply and bind came in picture

--- CallApplyBind.js
+++ CallApplyBind.js
@@ -43,3 +43,35 @@ let print = function(Lname, address){
 print.call(b, 'team', 'usa') // takes obj to be attached with and comma separated arguments 
 print.apply(a, ['team', 'usa']) // takes obj to be attached with and array of arguments 
 print.bind(b, 'team', 'usa')(); // takes obj to be attached with and array of arguments or array and return a function, hence to be called with brackets to get output
+
+/*
+https://www.codingame.com/playgrounds/9799/learn-solve-call-apply-and-bind-methods-in-javascript
+
+
+
+*/
+
+var car = { 
+    registrationNumber: "GA12345",
+    brand: "Toyota",
+  
+    displayDetails: function(){
+        console.log(this.registrationNumber + " " + this.brand);
+    }
+  }
+  
+  
+car.displayDetails(); // OP:  GA12345 Toyota
+
+//But what if we want to borrow a method?
+var myCarDetails =  car.displayDetails;
+myCarDetails(); // op: undefined undefined[becoz Well, this won’t work as the “this” will be now assigned to the global context which doesn’t have neither the registrationNumber nor the brand property.]
+
+var myCarDetails = car.displayDetails.bind(car); 
+myCarDetails(); // GA12345 Toyota
+/*
+The bind() Method
+For such cases we can use the ECMAScript 5 bind() method of the Function.prototype property. This means bind() can be used by every single function.
+The bind() method creates a new function where “this” refers to the parameter in the parenthesis in the above case “car”. This way the bind() method enables calling a function with a specified “this” value.
+
+*/
